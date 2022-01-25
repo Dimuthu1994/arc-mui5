b0e346e967c6dc9f6535e9d9198bc8170d035d60
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-01-25 09:14:36 +0530

    init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { CssBaseline } from "@mui/material";
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Header /> Hello
+    </>
+  );
+}
+
+export default App;
